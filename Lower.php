be46750ef4785e5129e149d40252bd9ad8dc1997
Nicolas KLEIN <popnikos@gmail.com>
2017-08-11 10:39:27 +0200

    [FIX] useless line of dump

--- Lower.php
+++ Lower.php
@@ -16,7 +16,6 @@ namespace Popnikos\StringNormalizer;
 class Lower extends AbstractNormalizer{
     public function normalize($str = '') {
         // Lower case string
-        echo json_decode("\u04d4\u00c6",false);
         return mb_strtolower($str);
     }
 }
